a8fe07f996f24d5229e4865f49d17b23271ebbd6
Eucalypt <eluvk.dev@gmail.com>
2025-12-19 16:13:34 +0800

    byebye old man.

--- lib/client/err.dart
+++
@@ -1,8 +0,0 @@
-import 'package:result_dart/result_dart.dart';
-
-enum ClientError {
-  unexpectedError,
-  internalError,
-}
-
-typedef ClientResult<T extends Object> = AsyncResult<T, ClientError>;
