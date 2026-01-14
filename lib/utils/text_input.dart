b6e6ac663c9a3adecdc79b7a78652806cbdef06b
Eucalypt <eluvk.dev@gmail.com>
2026-01-14 17:13:15 +0800

    .

--- lib/utils/text_input.dart
+++ lib/utils/text_input.dart
@@ -1,18 +1,104 @@
 import 'package:flutter/material.dart';
 
-enum InputTitleEnum { title, description }
+abstract interface class TitleInterface {
+  String get _title;
+  IconData get _icon;
+  Color get _color;
+}
 
-extension InputTitleExtension on InputTitleEnum {
-  String get title {
+enum SyncStoreInputMetaEnum implements TitleInterface {
+  address,
+  enableTunnel;
+
+  @override
+  Color get _color {
     switch (this) {
-      case InputTitleEnum.title:
-        return '名称';
-      case InputTitleEnum.description:
-        return '描述';
+      case SyncStoreInputMetaEnum.address:
+        return Colors.orange;
+      case SyncStoreInputMetaEnum.enableTunnel:
+        return Colors.blue;
     }
   }
 
-  IconData get icon {
+  @override
+  IconData get _icon {
+    switch (this) {
+      case SyncStoreInputMetaEnum.address:
+        return Icons.cloud;
+      case SyncStoreInputMetaEnum.enableTunnel:
+        return Icons.link;
+    }
+  }
+
+  @override
+  String get _title {
+    switch (this) {
+      case SyncStoreInputMetaEnum.address:
+        return '服务器地址';
+      case SyncStoreInputMetaEnum.enableTunnel:
+        return '启用加密隧道';
+    }
+  }
+}
+
+enum AppSettingMetaEnum implements TitleInterface {
+  themeMode,
+  language,
+  fontScale;
+
+  @override
+  Color get _color {
+    switch (this) {
+      case AppSettingMetaEnum.themeMode:
+        return Colors.purple;
+      case AppSettingMetaEnum.language:
+        return Colors.teal;
+      case AppSettingMetaEnum.fontScale:
+        return Colors.indigo;
+    }
+  }
+
+  @override
+  IconData get _icon {
+    switch (this) {
+      case AppSettingMetaEnum.themeMode:
+        return Icons.brightness_6;
+      case AppSettingMetaEnum.language:
+        return Icons.language;
+      case AppSettingMetaEnum.fontScale:
+        return Icons.format_size;
+    }
+  }
+
+  @override
+  String get _title {
+    switch (this) {
+      case AppSettingMetaEnum.themeMode:
+        return '主题模式';
+      case AppSettingMetaEnum.language:
+        return '语言';
+      case AppSettingMetaEnum.fontScale:
+        return '字体大小';
+    }
+  }
+}
+
+enum InputTitleEnum implements TitleInterface {
+  title,
+  description;
+
+  @override
+  Color get _color {
+    switch (this) {
+      case InputTitleEnum.title:
+        return Colors.blue;
+      case InputTitleEnum.description:
+        return Colors.green;
+    }
+  }
+
+  @override
+  IconData get _icon {
     switch (this) {
       case InputTitleEnum.title:
         return Icons.title;
@@ -21,12 +107,13 @@ extension InputTitleExtension on InputTitleEnum {
     }
   }
 
-  Color get color {
+  @override
+  String get _title {
     switch (this) {
       case InputTitleEnum.title:
-        return Colors.blue;
+        return '名称';
       case InputTitleEnum.description:
-        return Colors.green;
+        return '描述';
     }
   }
 }
@@ -43,7 +130,7 @@ class TextInputWidget extends StatelessWidget {
   });
   final focusNode = FocusNode();
 
-  final InputTitleEnum title;
+  final TitleInterface title;
   final String initialValue;
   final void Function(bool)? onFocusChange;
   final bool autoFocus;
@@ -64,18 +151,18 @@ class TextInputWidget extends StatelessWidget {
           child: Row(
             children: [
               Material(
-                color: title.color,
+                color: title._color,
                 borderRadius: BorderRadius.circular(8),
                 child: SizedBox(
                   height: 32,
                   width: 32,
-                  child: Center(child: Icon(title.icon, color: Colors.white)),
+                  child: Center(child: Icon(title._icon, color: Colors.white)),
                 ),
               ),
               const SizedBox(width: 12),
               Expanded(
                 // flex: 1,
-                child: Text(title.title, style: Theme.of(context).textTheme.bodyMedium),
+                child: Text(title._title, style: Theme.of(context).textTheme.bodyMedium),
               ),
               Flexible(
                 flex: 2,
@@ -95,6 +182,86 @@ class TextInputWidget extends StatelessWidget {
                     print(value);
                     onChanged(value);
                   },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UserDefinedInputWidget extends StatelessWidget {
+  const UserDefinedInputWidget({super.key, required this.title, required this.widget});
+  final TitleInterface title;
+  final Widget widget;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(6, 6, 10, 6),
+          child: Row(
+            children: [
+              Material(
+                color: title._color,
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox(
+                  height: 32,
+                  width: 32,
+                  child: Center(child: Icon(title._icon, color: Colors.white)),
+                ),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Text(title._title, style: Theme.of(context).textTheme.bodyMedium),
+                // flex: 1,
+              ),
+              Flexible(
+                flex: 2,
+                child: Align(alignment: Alignment.centerRight, child: widget),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BoolSelectorInputWidget extends StatelessWidget {
+  const BoolSelectorInputWidget({super.key, required this.title, required this.initialValue, required this.onChanged});
+  final TitleInterface title;
+  final bool initialValue;
+  final void Function(bool) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(6, 6, 10, 6),
+          child: Row(
+            children: [
+              Material(
+                color: title._color,
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox(
+                  height: 32,
+                  width: 32,
+                  child: Center(child: Icon(title._icon, color: Colors.white)),
+                ),
+              ),
+              const SizedBox(width: 12),
+              Expanded(child: Text(title._title, style: Theme.of(context).textTheme.bodyMedium)),
+              Flexible(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Switch(value: initialValue, onChanged: (newValue) => onChanged(newValue)),
                 ),
               ),
             ],
